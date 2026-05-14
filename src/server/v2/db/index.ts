b663f93bdefa6dd80224e7f9192cf4ed7191abf2
Ragav Kumar <27591832+ragav-kumar@users.noreply.github.com>
2026-05-14 14:47:21 -0700

    index files

--- src/server/v2/db/index.ts
+++ src/server/v2/db/index.ts
@@ -1,22 +1,4 @@
-export { createAppDatabase, getAppDatabasePath } from './database.js';
-export { createV2Orm } from './orm.js';
-export type {
-    Npc as ObjectModelNpc,
-    Run as ObjectModelRun,
-    RunAuditLog as ObjectModelRunAuditLog,
-    Session as ObjectModelSession,
-    SessionEntry as ObjectModelSessionEntry,
-    SessionEntryKind as ObjectModelSessionEntryKind,
-    SessionKind as ObjectModelSessionKind,
-    Setting as ObjectModelSetting,
-    RunKind as ObjectModelRunKind,
-    RunStatus as ObjectModelRunStatus,
-} from './objectModel.js';
-export type {
-    Npc as SchemaNpc,
-    Run as SchemaRun,
-    RunAuditLog as SchemaRunAuditLog,
-    Session as SchemaSession,
-    SessionEntry as SchemaSessionEntry,
-    Setting as SchemaSetting,
-} from './schema.js';
+export * from './database.js';
+export * from './orm.js';
+export * from './objectModel.js';
+export * as schema from './schema.js';
