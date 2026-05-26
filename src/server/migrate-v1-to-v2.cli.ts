4efad87f0944ab75863f3edca9b64881a746ea38
Ragav Kumar <27591832+ragav-kumar@users.noreply.github.com>
2026-05-26 15:35:21 -0700

    remove last bits of v1

--- src/server/migrate-v1-to-v2.cli.ts
+++
@@ -1,6 +0,0 @@
-// Migration files are for the v1 to v2 transition. These are the only files permitted to touch both codebases.
-// These should be deleted during the v1 purge.
-
-import { runMigrationCli } from './migrate-v1-to-v2.js';
-
-void runMigrationCli();
